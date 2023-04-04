9f33d0553bbc44b7cf92330151c04f682cfab8e8
ahoia <gordonng382804@gmail.com>
2023-04-04 09:49:31 +0800

    jenkin test suit, xmlrunner

--- src/KFC/tests/smoketests/testsuite.py
+++ src/KFC/tests/smoketests/testsuite.py
@@ -1,9 +1,9 @@
 import unittest
-import HtmlTestRunner
+#import HtmlTestRunner
 from xmlrunner import xmlrunner
-from loginpage.loginpage_test import LoginPageTest
-from profilepage.profilepage_test import ProfilePageTest
-from orderpage.order_shop_test import OrderTest
+from tests.loginpage.loginpage_test import LoginPageTest
+from tests.profilepage.profilepage_test import ProfilePageTest
+from tests.orderpage.order_shop_test import OrderTest
 # get all tests from SearchProductTest and HomePageTest class
 loginpage_test = unittest.TestLoader().loadTestsFromTestCase(LoginPageTest)
 profilepage_test = unittest.TestLoader().loadTestsFromTestCase(ProfilePageTest)
@@ -11,7 +11,7 @@ orderpage_test = unittest.TestLoader().loadTestsFromTestCase(OrderTest)
 # create a test suite combining search_test and home_page_test
 smoke_tests = unittest.TestSuite([loginpage_test,profilepage_test,orderpage_test])
 # run the suite
-runner = HtmlTestRunner.HTMLTestRunner(output='src/KFC/Reports',combine_reports=True,report_title='Test Report',descriptions='Smoke Tests')
-#runner = xmlrunner.XMLTestRunner(verbosity=2, output='test-reports')
+#runner = HtmlTestRunner.HTMLTestRunner(output='src/KFC/Reports',combine_reports=True,report_title='Test Report',descriptions='Smoke Tests')
+runner = xmlrunner.XMLTestRunner(verbosity=2, output='src/KFC/Reports/testreports')
 # run the suite using HTMLTestRunner
 runner.run(smoke_tests)
